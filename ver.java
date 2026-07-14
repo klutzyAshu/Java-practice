9c4ea67af4c53d9a6cc303297178a3177d3e1d43
Gaurishankar sen <aashusen2006@gmail.com>
2026-07-14 21:52:17 +0530

    java day 1 vt

---
+++ ver.java
@@ -0,0 +1,13 @@
+/*public class ver{
+    public static void main(String[] args){
+        int age = 19;
+        char name = 'a';
+        boolean isTrue = true;
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(isTrue);
+
+
+    }
+}
+*/
