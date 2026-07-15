e5d21ce3067422802db2bbe439d2aa5fc7a3a0ce
Gaurishankar sen <aashusen2006@gmail.com>
2026-07-15 16:22:35 +0530

    tasks day 1

---
+++ AdditionalTask.java
@@ -0,0 +1,5 @@
+public class AdditionalTask{
+    public static void main (String[] args){
+        System.out.println("Ashu");
+    }
+}
---
+++ ScannerExample.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class ScannerExample {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter username");
+        String userName = myObj.nextLine();
+        System.out.println("username is " + userName);
+    }
+}
