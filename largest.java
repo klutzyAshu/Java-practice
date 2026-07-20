ef3bb6a59abc77788bd3f9c259a1ff1dbb3049fa
Aashu12exe <aashusen2006@gmail.com>
2026-07-20 18:24:49 +0530

    Day7

---
+++ largest.java
@@ -0,0 +1,15 @@
+class largest{
+    public static void main(String[] args){
+        int a = 12;
+        int b = 22;
+        int c = 45;
+        System.out.println("given numbers");
+        if(a>b){
+            System.out.println("A is bigger then "+ b);
+        }
+        else{
+            System.out.println("b is bigger");
+            
+        }
+    }
+}
