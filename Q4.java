605d227f23ee8a7cbe8a7393dae740f180f6b35f
Gaurishankar sen <aashusen2006@gmail.com>
2026-07-26 10:19:35 +0530

    Day11

---
+++ Q4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Q4 {
+    public static void main(String[] arga){
+        int a=6;
+        for(int i = 1; i<= a; i++){
+            for(int j = 1;j<=i; j++){
+            System.out.print((i*j) +" ");
+
+        }
+        System.out.println();
+    }
+    
+}
+    
+}
