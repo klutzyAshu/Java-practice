a621808474e7af96ed3fb01b0821f408bdba16b7
Gaurishankar sen <aashusen2006@gmail.com>
2026-07-27 10:37:12 +0530

    Test-Ans

---
+++ test week 1/Q2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Q2{
+    public static void main(String[]args){
+    System.out.println("Enter your number ");
+    Scanner sc = new Scanner(System.in);
+    long n = sc.nextLong();
+        int[] count = new int[10];
+        long temp = Math.abs(n);
+        if(temp==0){
+            count[0] = 1;
+        }else{
+            while(temp >0 ){
+                int digit = (int) (temp %10);
+                count[digit]++;
+                temp /= 10;
+
+            }
+            System.out.println("Digit frequencies in "+n+ ":");
+            for (int i = 0 ; i < 10; i++){
+                if (count[i] > 0){
+                    System.out.println("Digit " + i + ": " + count[i] + " time(s)");
+
+                }
+
+            }
+
+
+        }
+    }
+}
