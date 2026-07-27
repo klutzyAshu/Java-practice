8247e50c0330cc8140a5b5ad41bd7ad34f359fab
Gaurishankar sen <aashusen2006@gmail.com>
2026-07-27 12:12:52 +0530

    Q4 Ans

--- test week 1/Q4.java
+++ test week 1/Q4.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Q4 {
     public static void main(String[] arga){
-        int a=6;
+        int a=1;
         for(int i = 1; i<= a; i++){
             for(int j = 1;j<=i; j++){
             System.out.print((i*j) +" ");
