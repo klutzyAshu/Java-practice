d17c452b9e1fb65993ae80f1945dfd2929db64b7
Gaurishankar sen <aashusen2006@gmail.com>
2026-07-27 23:27:30 +0530

    arr-practice

---
+++ Arrays/array2e1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class array2e1 {
+public static void main(String [] args){
+    System.out.println("Enter elements into the array ");
+    // Scanner sc = new Scanner(System.in);
+    int [] arr ={1 ,2 ,3 ,4 ,3 ,2 ,4 ,3 ,64 ,32 ,54 ,3 ,6 ,3 ,7 ,8 ,} ;
+    
+    // int sum = 0;
+    for(int i =0; i < arr.length; i++){
+        
+        System.out.println(arr[ i ]);
+    }
+        
+    }
+    
+}
---
+++ Arrays/array2e2.java
@@ -0,0 +1,14 @@
+public class array2e2 {
+    public static void main(String[] args){
+        int[] arr = {12,31,33,23,24,54,65,75,100,32};
+        int sum = 0;
+        for(int i=0;i<arr.length; i++){
+            sum = sum+arr[i];
+
+        }
+        System.out.println("sum = " + sum);
+    }
+}
+
+
+`
