bbee36ffb27f1c5ea9648d8908aeef878222cb1d
Aashu12exe <aashusen2006@gmail.com>
2026-08-10 14:41:11 +0530

    day1 orgnized *

---
+++ FunctionCalling/ExampleF1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ExampleF1{
+    static void print(){
+        
+        System.out.println("");
+
+    }
+    public static void main(String [] args){
+        print();
+        }
+        
+    }
