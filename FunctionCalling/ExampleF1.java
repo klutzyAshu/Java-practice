8802e5c5f30cb23f1235920b0041e0109ee690d1
Aashu12exe <aashusen2006@gmail.com>
2026-08-21 07:21:55 +0530

    hello

--- FunctionCalling/ExampleF1.java
+++ FunctionCalling/ExampleF1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class ExampleF1{
     static void print(){
         
-        System.out.println("Hello function ");
+        // System.out.println("Hello function ");
 
     }
     public static void main(String [] args){
