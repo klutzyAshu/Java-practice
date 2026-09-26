c4b6351efae713924f35d79ad5cfa65cf411da33
KlutzyAshu <aashusen2006@gmail.com>
2026-09-26 23:43:24 +0530

    star triangles

---
+++ core-java/BasicsJava/star.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class star{
+public static void main(String [] args){
+    System.out.println("Enter the numbers of ur wishes:");
+
+    Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i =1 ; i<n - i ; i++)
+            {
+            for(int j =1 ; j<n ; j++)
+                System.out.print(" ");
+            
+            for(int j =1 ; j<=(2*n-i) ; j++){
+    }
+
+        System.out.println("*");
+    }
+
+
+        }
+}
